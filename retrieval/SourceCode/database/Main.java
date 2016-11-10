37706e7e3e65c6b46f773798174a28af20d5bdad
Hà Viết Tráng <trang12596@gmail.com>
2016-11-10 15:18:03 +0700

    Add files via upload

---
+++ retrieval/SourceCode/database/Main.java
@@ -0,0 +1,14 @@
+package database;
+
+/**
+ * @author Hà Viết Tráng - HAVIETTRANG
+ * @date Nov 10, 2016 3:01:58 PM
+ * @website haviettrang.blogspot.com
+ * @Notes View my notes at haviettrang.postach.io
+ */
+public class Main {
+    private static final String RSS_TIN_TUC_VN = "http://tintuc.vn/rss/giao-thong.rss";
+    
+    
+    
+}
