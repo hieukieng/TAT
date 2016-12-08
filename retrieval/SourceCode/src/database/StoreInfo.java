8378e4a94111497b73e755525200710276b430a7
haviettrang <trang12596@gmail.com>
2016-12-08 13:06:53 +0700

    fix bug

--- retrieval/SourceCode/src/database/StoreInfo.java
+++ retrieval/SourceCode/src/database/StoreInfo.java
@@ -13,19 +13,19 @@ import news.Article;
  */
 public class StoreInfo {
 
-    private static final String IMAGE_URL =
-            "https://s-media-cache-ak0.pinimg.com/564x/69/2b/7f/692b7fdec925793d38b4dd90ffb6e384.jpg";
+    private static final String IMAGE_URL
+            = "https://s-media-cache-ak0.pinimg.com/564x/69/2b/7f/692b7fdec925793d38b4dd90ffb6e384.jpg";
     private TrafficAccident trafficAccident;
     private Connection connection;
     private PreparedStatement preSta;
     private String query;
 
     public StoreInfo() {
-        
+
         trafficAccident = new TrafficAccident();
-        
+
         trafficAccident.connectToDatabase();
-        
+
         connection = trafficAccident.getConnection();
     }
 
@@ -41,25 +41,33 @@ public class StoreInfo {
         preSta.setString(3, article.getUrl());
         preSta.setString(4, article.getDescription());
         preSta.setString(5, null);
-        
+
         if (article.getImageUrl() != null) {
             preSta.setString(6, article.getImageUrl());
         } else {
             preSta.setString(6, IMAGE_URL);
         }
-        
+
         preSta.setString(7, article.getContent());
 
         preSta.executeUpdate();
 
         System.out.println("Store information successfully");
+
+    }
+
+    public void stop() {
+        try {
+            preSta.close();
+        } catch (SQLException ex) {
+            ex.printStackTrace();
+        }
         
-        preSta.close();
         trafficAccident.disconnectToDatabase();
     }
-    
-    public <E extends Article, T extends  Collection> void storeInfo(E article, T links) throws SQLException {
-        
+
+    public <E extends Article, T extends Collection> void storeInfo(E article, T links) throws SQLException {
+
         for (Iterator iterator = links.iterator(); iterator.hasNext();) {
             String url = (String) iterator.next();
             article.parseUrl(url);
